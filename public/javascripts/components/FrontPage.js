434a476912ae579451ada9152ec2dc4ec0b437dd
Nick Janssen <info@nickjanssen.com>
2016-03-04 16:01:58 +0900

    add renderView, three.js

--- public/javascripts/components/FrontPage.js
+++ public/javascripts/components/FrontPage.js
@@ -1,10 +1,15 @@
 import React from 'react'
 
+import RenderView from './RenderView'
+
 export default React.createClass({
   render() {
     return (
       <div>
         SPA Application Boilerplate
+        <div>
+          <RenderView />
+        </div>
       </div>
     )
   }
