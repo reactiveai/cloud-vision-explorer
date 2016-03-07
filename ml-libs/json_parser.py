c2b85e66f37119fdab5e6da3455f165f54a92c85
philipperemy <premy@reactive.co.jp>
2016-03-07 20:39:28 +0900

    Adding TSNE C++ library as well as word2vec procesor, json parser and the MATLAB T-SNE wrapper

---
+++ ml-libs/json_parser.py
@@ -0,0 +1,34 @@
+from __future__ import print_function
+import json
+import word2vec
+JSON_FILENAME = "../data/out.json"
+
+
+def enrich_map_with_word2vec(label_map, w2v):
+    for idx, labels in label_map.iteritems():
+        for label in labels:
+            desc = label['description']
+            vec = word2vec.process_word(desc, w2v, silent=True)
+            label['word2vec'] = vec
+
+
+def generate_label_map():
+    with open(JSON_FILENAME) as data_file:
+        label_map = {}
+        data = json.load(data_file)['responses']
+        for idx, record in enumerate(data):
+            try:
+                label_map[idx] = record['labelAnnotations']
+            except KeyError:
+                pass
+    return label_map
+
+
+def main():
+    w2v = word2vec.load_glove(50)
+    label_map = generate_label_map()
+    enrich_map_with_word2vec(label_map, w2v)
+    return label_map
+
+if __name__ == "__main__":
+    main()
---
+++ ml-libs/low_dim_mapper2.py
@@ -0,0 +1,29 @@
+from __future__ import print_function
+import json_parser
+from scipy.io import savemat
+import string
+import word2vec
+
+if __name__ == "__main__":
+    vecs = []
+    label_names = []
+    label_map = json_parser.main()
+    for label in label_map.values():
+        # print("______________")
+
+        label_vectors = []
+        label_scores = []
+        label_desc = []
+        for val in label:  # use scores in a later version.
+            label_vectors.append(val['word2vec'])
+            label_scores.append(val['score'])
+            label_desc.append(str(''.join(c for c in val['description'] if c in string.printable)))
+        output_vec = word2vec.linear_combination_vectors(vectors=label_vectors, coefficients=label_scores)
+        vecs.append(output_vec)
+        label_names.append(label_desc[0] + "+") # otherwise its too big. Plus means we added something
+
+    matlab_dict = dict()
+    matlab_dict['data'] = vecs
+    matlab_dict['label'] = label_names
+    savemat(file_name="tsne.mat", appendmat=False, do_compression=False,oned_as='row', mdict=matlab_dict)
+
