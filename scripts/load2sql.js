e97019b51ce74262669b560842e0144ddc8847cb
Ray Sakai <ray.sakai@logical-fabrics.com>
2016-03-13 20:14:53 +0900

    supports large amount of files to be processed

--- scripts/load2sql.js
+++ scripts/load2sql.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 'use strict'
-const _     = require('lodash')
-const util  = require('util')
-const glob  = require('glob')
-const fs    = require('fs')
-const path  = require('path')
-const mysql = require('mysql')
+const _      = require('lodash')
+const util   = require('util')
+const glob   = require('glob')
+const fs     = require('fs')
+const path   = require('path')
+const mysql  = require('mysql')
+const events = require('events')
 
+const eventEmitter = new events.EventEmitter()
 const ID_LENGTH = 32
+const BATCH_SIZE = 5
 
 if(process.argv.length < 4) {
   console.log('usage  : load2sql <VisionPattern> <ThumbDir>')
@@ -25,32 +28,49 @@ const pool = mysql.createPool({
   database: process.env.MYSQL_DATABASE
 })
 
-_.each(glob.sync(VisionPattern), (file, index, files) => {
-  const id = path.basename(file, '.json')
-  if(id.length != ID_LENGTH) {
-    console.log(`weird! ${file}`)
+const runReplace = (file) => {
+  return new Promise((resolve, reject) => {
+    const id = path.basename(file, '.json')
+    if(id.length != ID_LENGTH) { return reject(file) }
+
+    const vision   = fs.readFileSync(file, 'utf8')
+    const thumb32  = fs.readFileSync(`${ThumbDir}/32x32/${id}.jpg`)
+    const thumb64  = fs.readFileSync(`${ThumbDir}/64x64/${id}.jpg`)
+    const thumb128 = fs.readFileSync(`${ThumbDir}/128x128/${id}.jpg`)
+    const thumb    = fs.readFileSync(`${ThumbDir}/150x150/${id}.jpg`) // Compatibility - will be removed
+
+    const dataset = {id, vision, thumb32, thumb64, thumb128, thumb}
+
+    pool.query('REPLACE INTO entries SET ?', dataset, (err, res) => {
+      if(err){
+        console.log(err)
+        return reject(`${id} : ${err}`)
+      }
+      console.log(id)
+      return resolve(id)
+    })
+  })
+}
+
+const files = glob.sync(VisionPattern)
+const runner = () => {
+  if(_.isEmpty(files)) {
+    console.log('done')
     pool.end()
-    process.exit()
     return
   }
 
-  const vision = fs.readFileSync(file, 'utf8')
-  const thumb  = fs.readFileSync(`${ThumbDir}/${id}.jpg`)
-
-  pool.query('REPLACE INTO entries SET ?', {id, vision, thumb}, (err, res) => {
-    let count = index + 1
-    console.log(`${id} : ${count} / ${files.length}`)
-    if(err){
-      console.log(err)
-      pool.end()
-      process.exit()
-    }
-
-    if(count == files.length) {
-      _.defer(() => {
-        pool.end()
-        console.log('Done.')
-      })
-    }
+  Promise.all(
+    _(files.splice(0, BATCH_SIZE))
+    .map((file) => { return runReplace(file) })
+    .value()
+  )
+  .then(() => { eventEmitter.emit('run') })
+  .catch((err) => {
+    console.log(err)
+    pool.end()
   })
-})
+}
+
+eventEmitter.on('run', runner)
+eventEmitter.emit('run')
