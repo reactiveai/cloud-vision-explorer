326e4afee9e45d65b40f02618ac9f3682825ec0d
Ray Sakai <ray.sakai@logical-fabrics.com>
2016-03-14 02:03:16 +0900

    promisfied

--- routes/ws.js
+++ routes/ws.js
@@ -1,18 +1,38 @@
 const _       = require('lodash')
 const numeral = require('numeral')
 
-const fetchThumb = (sock, db, ids, fieldName) => {
+const fetchThumb = (db, ids, fieldName) => {
   return new Promise((resolve, reject) => {
     const queryStr = 'SELECT ?? FROM entries WHERE id IN (?)'
-    const columns = [fieldName]
+    const columns = ['id', fieldName]
     db.query(queryStr, [columns, ids], (err, rows) => {
       if(err) { return reject(err) }
 
       const data = _.map(rows, (row) => {
-        console.log(`${fieldName} : ${row.id} / ${numeral(row[fieldName].byteLength).format('0.0 b')}`)
+        console.log(`${fieldName} : ${row['id']} / ${numeral(row[fieldName].byteLength).format('0.0 b')}`)
         return {
-          id: row.id,
+          id: row['id'],
           thumb: row[fieldName]
+        }
+      })
+
+      resolve(data)
+    })
+  })
+}
+
+const fetchVision = (db, ids) => {
+  return new Promise((resolve, reject) => {
+    const queryStr = 'SELECT ?? FROM entries WHERE id IN (?)'
+    const columns = ['id', 'vision']
+    db.query(queryStr, [columns, ids], (err, rows) => {
+      if(err) { return reject(err) }
+
+      const data = _.map(rows, (row) => {
+        console.log(`vision : ${row['id']} / ${numeral(row['vision'].byteLength).format('0.0 b')}`)
+        return {
+          id: row['id'],
+          vision: row['vision']
         }
       })
 
@@ -26,28 +46,16 @@ const onConnection = (sock, db) => {
 
   _.each(['thumb32', 'thumb64', 'thumb128'], (field) => {
     sock.on(field, (ids) => {
-      fetchThumb(sock, db, ids, field)
-      .then((data) => { sock.emit(field, data)})
+      fetchThumb(db, ids, field)
+      .then((data) => { sock.emit(field, data) })
       .catch((err) => { console.log(`ERROR : ${err}]`) })
     })
   })
 
   sock.on('vision', (ids) => {
-    const queryStr = 'SELECT id, vision FROM entries WHERE id IN (?)'
-    db.query(queryStr, [ids], (err, rows) => {
-      if(err) {
-        console.log(err)
-        return
-      }
-
-      sock.emit('vision', _.map(rows, (row) => {
-        console.log(`vision : ${row.id} / ${numeral(row.vision.length).format('0.0 b')}`)
-        return {
-          id: row['id'],
-          vision: JSON.parse(row['vision'])
-        }
-      }))
-    })
+    fetchVision(db, ids)
+    .then((data) => { sock.emit('vision', data) })
+    .catch((err) => { console.log(`ERROR : ${err}]`) })
   })
 }
 
