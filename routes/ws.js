7a7f809bf72c74e51d4f5531e03529e646368ac8
Ray Sakai <ray.sakai@logical-fabrics.com>
2016-03-14 00:35:48 +0900

    BREAKING CHANGE! new websocket API

--- routes/ws.js
+++ routes/ws.js
@@ -1,24 +1,34 @@
 const _       = require('lodash')
 const numeral = require('numeral')
 
+const emitThumb = (sock, db, ids, fieldName) => {
+  return new Promise((resolve, reject) => {
+    const queryStr = 'SELECT id, ?? FROM entries WHERE id IN (?)'
+    const columns = ['id', fieldName]
+    db.query(queryStr, [columns, ids], (err, rows) => {
+      if(err) { return reject(err) }
+
+      const data = _.map(rows, (row) => {
+        console.log(`${fieldName} : ${row.id} / ${numeral(row[fieldName].byteLength).format('0.0 b')}`)
+        return {
+          id: row['id'],
+          thumb: row[fieldName]
+        }
+      })
+
+      sock.emit(fieldName, data)
+      resolve(data)
+    })
+  })
+}
+
 const onConnection = (sock, db) => {
   console.log('connected')
 
-  sock.on('thumbnail', (ids) => {
-    const queryStr = 'SELECT id, thumb FROM entries WHERE id IN (?)'
-    db.query(queryStr, [ids], (err, rows) => {
-      if(err) {
-        console.log(err)
-        return
-      }
-
-      sock.emit('thumbnail', _.map(rows, (row) => {
-        console.log(`emitting thumbnail : ${row.id} / ${numeral(row.thumb.byteLength).format('0.0 b')}`)
-        return {
-          id: row['id'],
-          thumb: row['thumb']
-        }
-      }))
+  _.each(['thumb32', 'thumb64', 'thumb128'], (field) => {
+    sock.on(field, (ids) => {
+      emitThumb(sock, db, ids, field)
+      .catch((err) => { console.log(`ERROR : ${err}]`) })
     })
   })
 
@@ -31,7 +41,7 @@ const onConnection = (sock, db) => {
       }
 
       sock.emit('vision', _.map(rows, (row) => {
-        console.log(`emitting vision : ${row.id} / ${numeral(row.vision.length).format('0.0 b')}`)
+        console.log(`vision : ${row.id} / ${numeral(row.vision.length).format('0.0 b')}`)
         return {
           id: row['id'],
           vision: JSON.parse(row['vision'])
