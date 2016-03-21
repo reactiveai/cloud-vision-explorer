744d328cd1089178cc8dad82ede1ee234b7e54f9
Nick Janssen <info@nickjanssen.com>
2016-03-21 21:20:18 +0900

    RenderView: add hexagon masks to images

--- src/javascripts/components/RenderView.js
+++ src/javascripts/components/RenderView.js
@@ -427,8 +427,30 @@ export default React.createClass({
       image.src = `data:image/jpeg;base64,${b64img}`
 
       const texture = new THREE.Texture()
-      texture.image = image
+
+      const canvas = document.createElement('canvas')
+
       image.onload = function() {
+        canvas.width = image.width
+        canvas.height = image.height
+
+        const context = canvas.getContext('2d')
+
+        // Create a hexagon shape
+        context.beginPath()
+        context.lineTo(canvas.width / 9 * 2, 0)
+        context.lineTo(canvas.width / 9 * 7, 0)
+        context.lineTo(canvas.width, canvas.height / 2)
+        context.lineTo(canvas.width / 9 * 7, canvas.height)
+        context.lineTo(canvas.width / 9 * 2, canvas.height)
+        context.lineTo(0, canvas.height / 2)
+        context.closePath()
+        // Clip to the current path
+
+        context.clip()
+        context.drawImage(image, 0, 0)
+
+        texture.image = canvas
         texture.needsUpdate = true
       }
 
