22a05e336e651617b3bd1f65c83e7bb28e58b414
Yu Yamanaka <yyamanaka@reactive.co.jp>
2016-03-21 22:02:33 +0900

    Toggle highlighing of face landmarks

--- src/javascripts/actions/sidebar.js
+++ src/javascripts/actions/sidebar.js
@@ -1,6 +1,8 @@
 export const SHOW_SIDEBAR = 'SHOW_SIDEBAR'
 export const HIDE_SIDEBAR = 'HIDE_SIDEBAR'
 export const CHANGE_TAB = 'CHANGE_TAB'
+export const SHOW_HIGHLIGHT_FACE_LANDMARKS = 'SHOW_HIGHLIGHT_FACE_LANDMARKS'
+export const HIDE_HIGHLIGHT_FACE_LANDMARKS = 'HIDE_HIGHLIGHT_FACE_LANDMARKS'
 
 export function showSidebar() {
   return { type: SHOW_SIDEBAR }
@@ -12,4 +14,12 @@ export function hideSidebar() {
 
 export function changeTab(tabIndex) {
   return { type: CHANGE_TAB, tabIndex }
+}
+
+export function showHighlightFaceLandmarks() {
+  return { type: SHOW_HIGHLIGHT_FACE_LANDMARKS }
+}
+
+export function hideHighlightFaceLandmarks() {
+  return { type: HIDE_HIGHLIGHT_FACE_LANDMARKS }
 }
