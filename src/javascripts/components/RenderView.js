39dffa1cf39c1b297339b4b79519f9da17af2115
Nick Janssen <info@nickjanssen.com>
2016-03-22 17:26:30 +0900

    various: refactor bookmark & zoom ids into constants

--- src/javascripts/components/RenderView.js
+++ src/javascripts/components/RenderView.js
@@ -14,6 +14,7 @@ import _ from 'lodash'
 import Shaders from '../misc/Shaders.js'
 
 import { getVisionJsonURL, preloadImage } from '../misc/Util.js'
+import { OPEN_IMAGE_BOOKMARK_IDS, ZOOM_CLUSTER_BOOKMARK_IDS } from '../misc/Constants.js'
 
 import io from 'socket.io-client'
 
