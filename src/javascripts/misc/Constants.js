07cf3219bfa86a02d7cf36653fbaf1f489d78042
Nick Janssen <info@nickjanssen.com>
2016-03-22 17:26:30 +0900

    various: refactor bookmark & zoom ids into constants

---
+++ src/javascripts/misc/Constants.js
@@ -0,0 +1,19 @@
+
+
+module.exports = {
+  OPEN_IMAGE_BOOKMARK_IDS: [
+    {id: '24e144ee08ce069fd077904f9039a5f4' },  // Cat
+    {id: '8fc425eaa277db86186edd229b0e3591' },  // Kangaroo
+    {id: '22b8f17c84059d5d293278f47d1561bf' },  // Ethnic Clothes
+    {id: '0c68e487dd8f16f343341569c29733b3' },  // Citi Field
+    {id: '326c917aed041bc7926238973ec5d36b' }   // Android
+  ],
+
+  // These are actually also image IDs, because it's easy to reference them
+  // even though we're just using them to zoom to a particular part of a cluster
+  ZOOM_CLUSTER_BOOKMARK_IDS: [
+    {id: '32d0b59482f5796635fb6e9989283ad0' },  // Dog
+    {id: '53cdcef1380a64d255d8f2d93655aec6' },  // Plant
+    {id: 'b550497b9dd1937694ed7647620a258e' },  // Residental area
+  ]
+}
