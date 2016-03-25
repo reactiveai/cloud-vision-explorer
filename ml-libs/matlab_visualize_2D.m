17f2705e5a6e2cefbed94aaba4183e0c801b41e8
philipperemy <premy@reactive.co.jp>
2016-03-25 12:13:25 +0900

    matlab visualise 2D final

--- ml-libs/matlab_visualize_2D.m
+++ ml-libs/matlab_visualize_2D.m
@@ -1,4 +1,9 @@
 load('vectors.mat');
-scatter(vectors(:, 1), vectors(:,2));
+assignments = assignments';
+scatter(vectors(:, 1), vectors(:,2), [], assignments, 'filled', 'pentagram');
+colormap(jet);
 labels = cellstr(labels);
-text(vectors(:, 1), vectors(:,2), labels);
+t = text(vectors(:, 1), vectors(:,2), labels);
+for i = 1:length(t)
+    t(i).FontSize = 7;
+end
