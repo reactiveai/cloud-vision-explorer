c1858a52c132c21b9c7629a5b206244306e6646e
Ray Sakai <ray.sakai@logical-fabrics.com>
2016-03-29 14:38:26 +0900

    no more Cloud SQL on Server

--- routes/ws.js
+++ routes/ws.js
@@ -1,38 +1,32 @@
 const _       = require('lodash')
 const numeral = require('numeral')
+const request = require('superagent')
 
-const fetchThumb = (db, ids, fieldName) => {
-  return new Promise((resolve, reject) => {
-    if (!ids.length) {
-      return reject('No ids given')
-    }
+const THUMB_API2DIR = {
+  thumb32: '32x32',
+  thumb64: '64x64',
+  thumb128: '128x128'
+}
 
-    const queryStr = 'SELECT ?? FROM entries WHERE id IN (?)'
-    const columns = ['id', fieldName]
-    db.query(queryStr, [columns, ids], (err, rows) => {
-      if(err) { return reject(err) }
-
-      const data = _.map(rows, (row) => {
-        console.log(`${fieldName} : ${row['id']} / ${numeral(row[fieldName].byteLength).format('0.0 b')}`)
-        return {
-          id: row['id'],
-          thumb: row[fieldName]
-        }
-      })
-
-      resolve(data)
-    })
-  })
+const getThumbURL = (id, dirName) => {
+  return `https://storage.googleapis.com/${process.env.GCS_BUCKET}/thumbnail/${dirName}/${id}.jpg`
 }
 
 const onConnection = (sock, db) => {
   console.log('connected')
 
-  _.each(['thumb32', 'thumb64', 'thumb128'], (field) => {
-    sock.on(field, (ids, callback) => {
-      fetchThumb(db, ids, field)
-      .then((data) => callback(data))
-      .catch((err) => { console.log(`ERROR : ${err}`) })
+  _.each(THUMB_API2DIR, (dirName, apiName) => {
+    sock.on(apiName, (id, cb) => {
+      request
+      .get(getThumbURL(id, dirName))
+      .end((err, res) => {
+        if(err) {
+          console.log(err)
+          return
+        }
+        console.log(`${apiName} : ${id} / ${numeral(res.body.byteLength).format('0.0 b')}`)
+        cb(res.body)
+      })
     })
   })
 }
