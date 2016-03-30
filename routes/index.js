aa5be06c494683bd7ecd17c08e983f7d0ad888ee
Yu Yamanaka <yyamanaka@reactive.co.jp>
2016-03-30 18:54:50 +0900

    Refactor nodejs routes

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,17 @@ const express = require('express')
 const router  = express.Router()
 
 module.exports = (app) => {
-  const renderIndex = (req, res) => {
+
+  router.get('/wstest', (req, res) => {
+    res.render('wstest', {})
+  })
+
+  router.get('/*', (req, res) => {
     if (process.env.GCS_BUCKET_NAME) {
       res.render('index', { gcsBucketName: process.env.GCS_BUCKET_NAME })
     } else {
       res.status(500).send('GCS_BUCKET_NAME variable is required')
     }
-  }
-
-  router.get('/', renderIndex)
-  router.get('/galaxy', renderIndex)
-
-  router.get('/wstest', (req, res) => {
-    res.render('wstest', {})
   })
 
   return router
