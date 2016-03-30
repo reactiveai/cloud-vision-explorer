e8a1aba91e523912ae065dcd61348110c6031daf
Yu Yamanaka <yyamanaka@reactive.co.jp>
2016-03-30 19:02:50 +0900

    Redirect to the entrance page if a user acccesed to undefined url

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import { Provider } from 'react-redux'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, Redirect, browserHistory } from 'react-router'
 import { syncHistoryWithStore } from 'react-router-redux'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
@@ -19,6 +19,7 @@ ReactDom.render(
     <Router history={history}>
       <Route path="/galaxy" component={FrontPage} />
       <Route path="/" component={EntrancePage} />
+      <Redirect from="*" to="/" />
     </Router>
   </Provider>,
   document.getElementById('contents')
