7f1edcb69bc26c4717a8125ec5980087cb6e08ef
Yu Yamanaka <yyamanaka@reactive.co.jp>
2016-04-01 17:31:50 +0900

    Load scss instead of sass

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,6 @@ const path              = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const autoprefixer      = require('autoprefixer')
 
-const sassLoaders = [
-  'css-loader',
-  'postcss-loader',
-  `sass-loader?indentedSyntax=sass&includePaths[]=${path.resolve(__dirname, './src')}`
-]
-
 let entryPoint = null
 if(_.isEqual(process.env.NODE_ENV, 'production')) {
   entryPoint = [
@@ -40,14 +34,16 @@ module.exports = {
         loaders: [ 'react-hot', 'babel' ],
         exclude: /node_modules/,
         include: __dirname,
-        test: /\.js[x]?$/
-      },
-      {
-        test: /\.sass$/,
-        loader: ExtractTextPlugin.extract('style-loader', sassLoaders.join('!'))
+        test: /\.jsx?$/
       },
       {
         test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!sass?sourceMap')
+      },
+      {
+        test: /\.scss$/,
+        include: /node_modules\/react-toolbox/,
         loader: ExtractTextPlugin.extract('style', 'css?sourceMap&modules&importLoaders=1!postcss!sass?sourceMap!toolbox')
       },
       {
@@ -71,8 +67,12 @@ module.exports = {
     })
   ],
 
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, './src')]
+  },
+
   resolve: {
-    extensions: ['', '.js', '.sass', '.scss', '.svg'],
+    extensions: ['', '.js', '.scss', '.svg'],
     modulesDirectories: ['src', 'node_modules']
   }
 }
