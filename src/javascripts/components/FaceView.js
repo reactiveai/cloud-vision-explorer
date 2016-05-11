2974e5e3e6cd6f8cf6240ce4342bdd6048a3fca3
Nick Janssen <info@nickjanssen.com>
2016-05-11 12:37:22 +0900

    FaceView: separate into FaceRenderer and just render to an image so we can reuse the same webgl canvas, use a smiley instead of actual 3d head (looks better)

--- src/javascripts/components/FaceView.js
+++ src/javascripts/components/FaceView.js
@@ -5,10 +5,12 @@ import THREE    from 'three'
 // Load some webpack-incompatible modules
 require('../misc/OBJLoader.js')(THREE)
 
+import getBase64FaceImage from '../misc/FaceRenderer.js'
+
 export default React.createClass({
   render() {
     return (
-      <div ref={(c) => this._container = c} className={this.props.className}></div>
+      <img ref={(c) => this._image = c} className={this.props.className} />
     )
   },
 
@@ -26,60 +28,9 @@ export default React.createClass({
   },
 
   componentDidMount() {
-
-    const camera = new THREE.PerspectiveCamera(70, this._container.clientWidth / this._container.clientHeight, 1, 1000)
-    camera.position.z = 3
-
-    const scene = new THREE.Scene()
-
-    const renderer = new THREE.WebGLRenderer({ alpha: true })
-    renderer.setPixelRatio(window.devicePixelRatio)
-    renderer.setSize(this._container.clientWidth, this._container.clientHeight)
-    this._container.appendChild(renderer.domElement)
-
-    const ambient_light = new THREE.AmbientLight(0x333333)
-    scene.add(ambient_light)
-
-    const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5)
-    directionalLight.position.set(0, -1, 0)
-    scene.add(directionalLight)
-
-    const point_light = new THREE.PointLight(0xffffff)
-    point_light.position.x = 2
-    point_light.position.y = 5
-    point_light.position.z = 10
-    scene.add(point_light)
-
-    const group = new THREE.Object3D()
-    scene.add(group)
-
-    group.rotation.x = THREE.Math.degToRad(-this.props.tiltAngle)
-    group.rotation.y = THREE.Math.degToRad(this.props.panAngle)
-    group.rotation.z = THREE.Math.degToRad(-this.props.rollAngle)
-
-    const loader = new THREE.OBJLoader()
-
-    loader.load(
-      'models/head.obj',
-      (object) => {
-        object.traverse((child) => {
-          if ( child instanceof THREE.Mesh ) {
-            child.material = new THREE.MeshPhongMaterial({
-              color: this.props.faceColor || 0xff0000
-            })
-          }
-        })
-
-        group.add(object)
-      }
-    )
-
-    const animate = () => {
-      requestAnimationFrame(animate)
-
-      renderer.render(scene, camera)
-    }
-
-    animate()
+    this._image.src = getBase64FaceImage(this.props.tiltAngle,
+      this.props.panAngle,
+      this.props.rollAngle,
+      this.props.faceColor)
   }
 })
