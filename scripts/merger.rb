a8415afa5e57e556bc011920d56d253803100058
Ray Sakai <ray.sakai@logical-fabrics.com>
2016-05-19 12:30:28 +0900

    fixed JSON formatting

--- scripts/merger.rb
+++ scripts/merger.rb
@@ -1,21 +1,19 @@
 #!/usr/bin/env ruby
 require 'json'
 
-output = File.open('vision_api.json', 'w')
 inputs = Dir.glob("results/*.json")
 input_count = inputs.length
 
-output.puts '['
+puts '['
 
 inputs.each_with_index{|file, index|
   image_id = File.basename(file, '.json')
 
   record = JSON.parse(File.read(file))
-	record['responses'][0]['imageId'] = image_id
+  record['responses'][0]['imageId'] = image_id
 
-	output.puts record.to_json
-	output.puts ',' if index != input_count - 1
+	puts record['responses'][0].to_json
+	puts ',' if index != input_count - 1
 }
 
-output.puts ']'
-output.close
+puts ']'
